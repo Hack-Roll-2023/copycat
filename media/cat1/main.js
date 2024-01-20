058d1b386d76133ca28d1a8174f26f91b7adddaf
9teMare <wang.zihao@u.nus.edu>
2024-01-20 22:48:49 +0800

    fix: dynamic cat walk

--- media/cat1/main.js
+++ media/cat1/main.js
@@ -1,5 +1,5 @@
 // const path = require("path");
-const catContainer = document.getElementById('cat-container');
+// const catContainer = document.getElementById("cat-container");
 const canvas = document.getElementById("anim_canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,104 +7,105 @@ const spriteImg = new Image();
 
 spriteImg.src = window.imgUri;
 
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
-
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// let canvas.width = (canvas.width = 600);
+// let canvas.height = (canvas.height = 600);
 
 const SPRITE_WIDTH = 32;
 const SPRITE_HEIGHT = 32;
 
 const FRAME_PER_SPRITE = 6;
 
-
 const sprite_details = {
-    "sit": {
-        "start_x": 33,
-        "start_y": 0,
-        "sprite_num": 7,
-        "is_looping": false,
-        "frames_per_sprite": 12,
-        "custom_idx_lst": [],
-        "x_inc": 0,
-        "y_inc": 0,
+    sit: {
+        start_x: 33,
+        start_y: 0,
+        sprite_num: 7,
+        is_looping: false,
+        frames_per_sprite: 12,
+        custom_idx_lst: [],
+        x_inc: 0,
+        y_inc: 0,
     },
-    "look_around": {
-        "start_x": 33,
-        "start_y": 128,
-        "sprite_num": 5,
-        "is_looping": true,
-        "frames_per_sprite": 12,
-        "custom_idx_lst": [2, 1, 0, 0, 1, 2, 2, 2, 3, 4, 4, 3, 2, 2, 2, 2],
-        "x_inc": 0,
-        "y_inc": 0,
+    look_around: {
+        start_x: 33,
+        start_y: 128,
+        sprite_num: 5,
+        is_looping: true,
+        frames_per_sprite: 12,
+        custom_idx_lst: [2, 1, 0, 0, 1, 2, 2, 2, 3, 4, 4, 3, 2, 2, 2, 2],
+        x_inc: 0,
+        y_inc: 0,
     },
 
+    lay_down: {},
 
-    "lay_down": {},
-
-
-    "walk_left": {
-        "start_x": 32 * 5 + 1,
-        "start_y": 32 * 12,
-        "sprite_num": 4,
-        "is_looping": true,
-        "frames_per_sprite": 8,
-        "custom_idx_lst": [],
-        "x_inc": 0,
-        "y_inc": -1,
+    walk_left: {
+        start_x: 32 * 5 + 1,
+        start_y: 32 * 12,
+        sprite_num: 4,
+        is_looping: true,
+        frames_per_sprite: 8,
+        custom_idx_lst: [],
+        x_inc: 0,
+        y_inc: -1,
     },
-    "walk_right": {
-        "start_x": 32 * 13 + 1,
-        "start_y": 32 * 12,
-        "sprite_num": 4,
-        "is_looping": true,
-        "frames_per_sprite": 8,
-        "custom_idx_lst": [],
-        "x_inc": 0,
-        "y_inc": 1,
+    walk_right: {
+        start_x: 32 * 13 + 1,
+        start_y: 32 * 12,
+        sprite_num: 4,
+        is_looping: true,
+        frames_per_sprite: 8,
+        custom_idx_lst: [],
+        x_inc: 0,
+        y_inc: 1,
     },
-    "walk_up": {
-        "start_x": 32 * 9 + 1,
-        "start_y": 32 * 12,
-        "sprite_num": 4,
-        "is_looping": true,
-        "frames_per_sprite": 8,
-        "custom_idx_lst": [],
-        "x_inc": -1,
-        "y_inc": 0,
+    walk_up: {
+        start_x: 32 * 9 + 1,
+        start_y: 32 * 12,
+        sprite_num: 4,
+        is_looping: true,
+        frames_per_sprite: 8,
+        custom_idx_lst: [],
+        x_inc: -1,
+        y_inc: 0,
     },
-    "walk_down": {
-        "start_x": 32 + 1,
-        "start_y": 32 * 12,
-        "sprite_num": 4,
-        "is_looping": true,
-        "frames_per_sprite": 8,
-        "custom_idx_lst": [],
-        "x_inc": 1,
-        "y_inc": 0,
+    walk_down: {
+        start_x: 32 + 1,
+        start_y: 32 * 12,
+        sprite_num: 4,
+        is_looping: true,
+        frames_per_sprite: 8,
+        custom_idx_lst: [],
+        x_inc: 1,
+        y_inc: 0,
     },
-}
-
+};
 
 // parameters
 let animName = null;
 let startX = null;
 let startY = null;
 let spriteNum = null;
-let currFrame = null;
+let currFrame = 0;
 let isLooping = null;
 let framePerSprite = null;
 let spriteIdxLst = [];
 
 let animRequest = null;
 
-let canvasPosX = 320;
-let canvasPosY = 320;
+let canvasPosX = 0; //canvas.height / 2;
+let canvasPosY = 0; //canvas.width / 2;
 let canvasXInc;
 let canvasYInc;
 
 function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    if (animRequest !== null) {
+        cancelAnimationFrame(animRequest);
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     let spriteIdx;
     if (isLooping) {
@@ -114,14 +115,9 @@ function animate() {
         } else {
             spriteIdx = Math.floor(currFrame / framePerSprite) % spriteNum;
         }
+    } else {
+        spriteIdx = Math.min(Math.floor(currFrame / framePerSprite), spriteNum - 1);
     }
-    else {
-        spriteIdx = Math.min(
-            Math.floor(currFrame / framePerSprite),
-            spriteNum - 1
-        );
-    }
-
 
     // Calculate locations based on idx
     const currX = startX + 32 * Math.floor(spriteIdx / 4);
@@ -130,29 +126,23 @@ function animate() {
     // Draw
     ctx.drawImage(
         spriteImg,
-        currY, currX, SPRITE_WIDTH, SPRITE_HEIGHT,          // this is for sprite
-        canvasPosY, canvasPosX, 5 * SPRITE_WIDTH, 5 * SPRITE_HEIGHT                   // this is for canvas
+        currY,
+        currX,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT, // this is for sprite
+        canvasPosY,
+        canvasPosX,
+        5 * SPRITE_WIDTH,
+        5 * SPRITE_HEIGHT // this is for canvas
     );
+
     currFrame++;
-    canvasPosX = Math.min(
-        Math.max(
-            0,
-            canvasPosX + canvasXInc
-        ),
-        CANVAS_WIDTH - 5 * SPRITE_WIDTH,
-    )
-    canvasPosY = Math.min(
-        Math.max(
-            0,
-            canvasPosY + canvasYInc
-        ),
-        CANVAS_HEIGHT - 5 * SPRITE_HEIGHT
-    );
+    canvasPosX = Math.min(Math.max(0, canvasPosX + canvasXInc), canvas.height - 5 * SPRITE_WIDTH);
+    canvasPosY = Math.min(Math.max(0, canvasPosY + canvasYInc), canvas.width - 5 * SPRITE_HEIGHT);
     animRequest = requestAnimationFrame(animate);
 }
 
 function playAnimation(newAnimName) {
-
     console.table("hi bitch", newAnimName);
     // Reset animation and save params
     let animName = newAnimName;
@@ -167,50 +157,11 @@ function playAnimation(newAnimName) {
     canvasXInc = sprite_details[animName]["x_inc"];
     canvasYInc = sprite_details[animName]["y_inc"];
 
-    if (animRequest != null) {
-        cancelAnimationFrame(animRequest);
-    }
-
     animate();
 }
 
-
-// function playAnimation(animation) {
-//     const frameCount = 10; // Adjust based on the number of frames in your sprite sheet
-//     let frame = 0;
-//
-//     function updateFrame() {
-//         catContainer.style.backgroundPosition = `-${frame * 100}px 0`; // Adjust the width of each frame
-//         frame = (frame + 1) % frameCount;
-//     }
-//
-//     function play() {
-//         updateFrame();
-//         requestAnimationFrame(play);
-//     }
-//
-//     switch (animation) {
-//         case 'idle':
-//             // Define the range of frames for the idle animation in your sprite sheet
-//             frame = 0;
-//             break;
-//         case 'walk':
-//             // Define the range of frames for the walk animation in your sprite sheet
-//             frame = 0;
-//             break;
-//         case 'jump':
-//             // Define the range of frames for the jump animation in your sprite sheet
-//             frame = 0;
-//             break;
-//         default:
-//             console.error('Unknown animation:', animation);
-//             return;
-//     }
-//
-//     play();
-// }
-
-class AnimCharacter {
-    //
-
-}
+window.addEventListener("resize", () => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    animate();
+});
--- src/extension.ts
+++ src/extension.ts
@@ -10,123 +10,122 @@ let copyCount = 0;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	function copyToClipboard() {
-		var editor = vscode.window.activeTextEditor;
-		var selection = editor!.selection;
+    function copyToClipboard() {
+        var editor = vscode.window.activeTextEditor;
+        var selection = editor!.selection;
 
-		var s = selection.start;
-		var e = selection.end;
+        var s = selection.start;
+        var e = selection.end;
 
-		var hasSelection = s.line !== e.line || s.character !== e.character;
+        var hasSelection = s.line !== e.line || s.character !== e.character;
 
-		if (hasSelection && editor!.selections.length === 1) {
-			// previousClipboard = clipboard;
-			clipboard = editor!.document.getText(new vscode.Range(s, e));
-			previousClipboard = clipboard;
-		}
-	}
+        if (hasSelection && editor!.selections.length === 1) {
+            // previousClipboard = clipboard;
+            clipboard = editor!.document.getText(new vscode.Range(s, e));
+            previousClipboard = clipboard;
+        }
+    }
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.copy", function () {
-			copyToClipboard();
-			var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
-			vscode.commands.executeCommand(copyCommand);
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.copy", function () {
+            copyToClipboard();
+            var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
+            vscode.commands.executeCommand(copyCommand);
+        })
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.cut", function () {
-			copyToClipboard();
-			var cutCommand = vscode.workspace.getConfiguration("copycat").cutCommand;
-			vscode.commands.executeCommand(cutCommand);
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.cut", function () {
+            copyToClipboard();
+            var cutCommand = vscode.workspace.getConfiguration("copycat").cutCommand;
+            vscode.commands.executeCommand(cutCommand);
+        })
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.undo", function () {
-			if (previousClipboard) {
-				clipboard = previousClipboard;
-				vscode.env.clipboard.writeText(previousClipboard);
-				previousClipboard = undefined;
-			}
-			var undoCommand = vscode.workspace.getConfiguration("copycat").undoCommand;
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.undo", function () {
+            if (previousClipboard) {
+                clipboard = previousClipboard;
+                vscode.env.clipboard.writeText(previousClipboard);
+                previousClipboard = undefined;
+            }
+            var undoCommand = vscode.workspace.getConfiguration("copycat").undoCommand;
 
-			vscode.commands.executeCommand(undoCommand);
-		})
-	);
+            vscode.commands.executeCommand(undoCommand);
+        })
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.paste", function () {
-			vscode.env.clipboard.readText().then(function (systemClipboard) {
-				var pasteCommand = vscode.workspace.getConfiguration("copycat").pasteCommand;
-				var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.paste", function () {
+            vscode.env.clipboard.readText().then(function (systemClipboard) {
+                var pasteCommand = vscode.workspace.getConfiguration("copycat").pasteCommand;
+                var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
 
-				if (clipboard === systemClipboard) {
-					vscode.commands.executeCommand(pasteCommand);
-				} else {
-					// vscode.commands.executeCommand(pasteCommand);
-					vscode.commands.executeCommand("copycat.colorsView.focus");
-					vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup');
-					copyCount += 1;
+                if (clipboard === systemClipboard) {
+                    vscode.commands.executeCommand(pasteCommand);
+                } else {
+                    // vscode.commands.executeCommand(pasteCommand);
+                    vscode.commands.executeCommand("copycat.colorsView.focus");
+                    vscode.commands.executeCommand("workbench.action.focusActiveEditorGroup");
+                    copyCount += 1;
 
-					provider.addCopyCounter(copyCount);
-					vscode.window.showInformationMessage(`Fk off copycat! ${systemClipboard}`);
-				}
-			});
-		})
-	);
+                    provider.addCopyCounter(copyCount);
+                    vscode.window.showInformationMessage(`Fk off copycat! ${systemClipboard}`);
+                }
+            });
+        })
+    );
 
-	const provider = new ColorsViewProvider(context.extensionUri);
+    const provider = new ColorsViewProvider(context.extensionUri);
 
-	context.subscriptions.push(vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.addColor", () => {
-			provider.addColor();
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.addColor", () => {
+            provider.addColor();
+        })
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.clearColors", () => {
-			provider.clearColors();
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.clearColors", () => {
+            provider.clearColors();
+        })
+    );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand("copycat.addCopyCounter", (count: number) => {
-			provider.addCopyCounter(count);
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("copycat.addCopyCounter", (count: number) => {
+            provider.addCopyCounter(count);
+        })
+    );
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 class ColorsViewProvider implements vscode.WebviewViewProvider {
-	public static readonly viewType = "copycat.colorsView";
+    public static readonly viewType = "copycat.colorsView";
 
-	private _view?: vscode.WebviewView;
+    private _view?: vscode.WebviewView;
 
-	constructor(private readonly _extensionUri: vscode.Uri) { }
+    constructor(private readonly _extensionUri: vscode.Uri) {}
 
-	private getWebviewContent(webview: vscode.Webview) {
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "main.js"));
+    private getWebviewContent(webview: vscode.Webview) {
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "main.js"));
 
-		// Do the same for the stylesheet.
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "reset.css"));
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css"));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "main.css"));
+        // Do the same for the stylesheet.
+        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "reset.css"));
+        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css"));
+        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "main.css"));
 
+        const catScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "cat1", "main.js"));
+        const catImgUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "cat1", "media", "black_1.png"));
+        // Do the same for the stylesheet.
 
-		const catScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "cat1", "main.js"));
-		const catImgUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "cat1", "media", "black_1.png"));
-		// Do the same for the stylesheet.
+        // Use a nonce to only allow a specific script to be run.
+        const nonce1 = getNonce();
+        const nonce2 = getNonce();
 
-		// Use a nonce to only allow a specific script to be run.
-		const nonce1 = getNonce();
-		const nonce2 = getNonce();
-
-		return `<!DOCTYPE html>
+        return `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -141,7 +140,7 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
 		
         <body>
             <h1 class="copyCounter">You copied 0 time</h1>
-
+			
             
 			<div id="buttons">
 				<button onclick="playAnimation('sit')">Sit</button>
@@ -162,55 +161,55 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
         </body>
 			
         </html>`;
-	}
+    }
 
-	public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, _token: vscode.CancellationToken) {
-		this._view = webviewView;
+    public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, _token: vscode.CancellationToken) {
+        this._view = webviewView;
 
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
 
-			localResourceRoots: [this._extensionUri],
-		};
+            localResourceRoots: [this._extensionUri],
+        };
 
-		webviewView.webview.html = this.getWebviewContent(webviewView.webview);
+        webviewView.webview.html = this.getWebviewContent(webviewView.webview);
 
-		webviewView.webview.onDidReceiveMessage((data) => {
-			switch (data.type) {
-				case "colorSelected": {
-					vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-					break;
-				}
-			}
-		});
-	}
+        webviewView.webview.onDidReceiveMessage((data) => {
+            switch (data.type) {
+                case "colorSelected": {
+                    vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+                    break;
+                }
+            }
+        });
+    }
 
-	public addColor() {
-		if (this._view) {
-			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
-			this._view.webview.postMessage({ type: "addColor" });
-		}
-	}
+    public addColor() {
+        if (this._view) {
+            this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+            this._view.webview.postMessage({ type: "addColor" });
+        }
+    }
 
-	public clearColors() {
-		if (this._view) {
-			this._view.webview.postMessage({ type: "clearColors" });
-		}
-	}
+    public clearColors() {
+        if (this._view) {
+            this._view.webview.postMessage({ type: "clearColors" });
+        }
+    }
 
-	public addCopyCounter(count: number) {
-		if (this._view) {
-			this._view.webview.postMessage({ type: "addCopyCount", value: count });
-		}
-	}
+    public addCopyCounter(count: number) {
+        if (this._view) {
+            this._view.webview.postMessage({ type: "addCopyCount", value: count });
+        }
+    }
 }
 
 function getNonce() {
-	let text = "";
-	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
