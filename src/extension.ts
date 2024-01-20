eee5e0a5f3571e82111943cd6d04f14b4b348094
ddx-510 <ddx0510@gmail.com>
2024-01-20 17:36:24 +0800

    feat: add cat image and focus when ctrl v

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ let clipboard: any;
 let previousClipboard: any;
 
 function getWebviewContent() {
-    return `<!DOCTYPE html>
+	return `<!DOCTYPE html>
   <html lang="en">
   <head>
 	  <meta charset="UTF-8">
@@ -22,70 +22,73 @@ function getWebviewContent() {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    function copyToClipboard() {
-        var editor = vscode.window.activeTextEditor;
-        var selection = editor!.selection;
+	function copyToClipboard() {
+		var editor = vscode.window.activeTextEditor;
+		var selection = editor!.selection;
 
-        var s = selection.start;
-        var e = selection.end;
+		var s = selection.start;
+		var e = selection.end;
 
-        var hasSelection = s.line !== e.line || s.character !== e.character;
+		var hasSelection = s.line !== e.line || s.character !== e.character;
 
-        if (hasSelection && editor!.selections.length === 1) {
-            // previousClipboard = clipboard;
-            clipboard = editor!.document.getText(new vscode.Range(s, e));
-            previousClipboard = clipboard;
-        }
-    }
+		if (hasSelection && editor!.selections.length === 1) {
+			// previousClipboard = clipboard;
+			clipboard = editor!.document.getText(new vscode.Range(s, e));
+			previousClipboard = clipboard;
+		}
+	}
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("copycat.copy", function () {
-            copyToClipboard();
-            var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
-            vscode.commands.executeCommand(copyCommand);
-        })
-    );
+	context.subscriptions.push(
+		vscode.commands.registerCommand("copycat.copy", function () {
+			copyToClipboard();
+			var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
+			vscode.commands.executeCommand(copyCommand);
+		})
+	);
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("copycat.cut", function () {
-            copyToClipboard();
-            var cutCommand = vscode.workspace.getConfiguration("copycat").cutCommand;
-            vscode.commands.executeCommand(cutCommand);
-        })
-    );
+	context.subscriptions.push(
+		vscode.commands.registerCommand("copycat.cut", function () {
+			copyToClipboard();
+			var cutCommand = vscode.workspace.getConfiguration("copycat").cutCommand;
+			vscode.commands.executeCommand(cutCommand);
+		})
+	);
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("copycat.undo", function () {
-            if (previousClipboard) {
-                clipboard = previousClipboard;
-                vscode.env.clipboard.writeText(previousClipboard);
-                previousClipboard = undefined;
-            }
-            var undoCommand = vscode.workspace.getConfiguration("copycat").undoCommand;
+	context.subscriptions.push(
+		vscode.commands.registerCommand("copycat.undo", function () {
+			if (previousClipboard) {
+				clipboard = previousClipboard;
+				vscode.env.clipboard.writeText(previousClipboard);
+				previousClipboard = undefined;
+			}
+			var undoCommand = vscode.workspace.getConfiguration("copycat").undoCommand;
 
-            vscode.commands.executeCommand(undoCommand);
-        })
-    );
+			vscode.commands.executeCommand(undoCommand);
+		})
+	);
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("copycat.paste", function () {
-            vscode.env.clipboard.readText().then(function (systemClipboard) {
-                var pasteCommand = vscode.workspace.getConfiguration("copycat").pasteCommand;
-                var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
+	context.subscriptions.push(
+		vscode.commands.registerCommand("copycat.paste", function () {
+			vscode.env.clipboard.readText().then(function (systemClipboard) {
+				var pasteCommand = vscode.workspace.getConfiguration("copycat").pasteCommand;
+				var copyCommand = vscode.workspace.getConfiguration("copycat").copyCommand;
 
-                if (clipboard === systemClipboard) {
-                    vscode.commands.executeCommand(pasteCommand);
-                } else {
-                    // debug("Using external clipboard content");
-                    // vscode.commands.executeCommand(pasteCommand);
-                    vscode.window.showInformationMessage(`Fk off copycat! ${systemClipboard}`);
+				if (clipboard === systemClipboard) {
+					vscode.commands.executeCommand(pasteCommand);
+				} else {
+					// debug("Using external clipboard content");
+					// vscode.commands.executeCommand(pasteCommand);
+					vscode.window.showInformationMessage(`Fk off copycat! ${systemClipboard}`);
+					// vscode.commands.executeCommand('workbench.view.explorer');
 
-                    const panel = vscode.window.createWebviewPanel("catCoding", "Cat Coding", vscode.ViewColumn.One, {});
-                    panel.webview.html = getWebviewContent();
-                }
-            });
-        })
-    );
+					// focus on the cat panel
+					vscode.commands.executeCommand("copycat.colorsView.focus");
+					const panel = vscode.window.createWebviewPanel("catCoding", "Cat Coding", vscode.ViewColumn.One, {});
+					panel.webview.html = getWebviewContent();
+				}
+			});
+		})
+	);
 
 	const provider = new ColorsViewProvider(context.extensionUri);
 
